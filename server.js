cc66d1e1b36aa983588ef44b95327a21ffac2f36
Eman <emanabukhater2001@gmail.com>
2025-03-15 21:30:32 +0300

    start socket.io connection and start server

--- server.js
+++ server.js
@@ -9,5 +9,30 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
+// set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+// socket.io connection
+io.on("connection", (socket) => {
+    console.log("A user connected");
+
+    socket.on("chatMessage", (chatData) => {
+        io.emit("chatMessage", chatData); // Send message to everyone
+    });
+
+    socket.on("disconnect", () => {
+        console.log("A user disconnected");
+    });
+});
+
+
+// set port
+const PORT = 3000;
+
+// start server
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
 
 
