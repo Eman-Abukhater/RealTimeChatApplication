d6f85739e90f665ed8c387b6344abd38863e78cf
Eman <emanabukhater2001@gmail.com>
2025-03-15 20:40:58 +0300

    start with import modules and initialize the server

---
+++ server.js
@@ -0,0 +1,13 @@
+// import modules
+const express = require('express');
+const http = require('http');
+const { Server } = require("socket.io");
+const path = require('path');
+
+// initialize express app
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+
+
